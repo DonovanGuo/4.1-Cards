7415fbe47b00d998236d392e01b9fd1a1c6a821f
BT_1N3_11 <BT_1N3_11@BT_1N3_11.bktech.local>
2018-03-15 09:19:10 -0400

    jjfish

--- 4.1 Cards/src/Deck.java
+++ 4.1 Cards/src/Deck.java
@@ -1,4 +1,37 @@
+import java.util.ArrayList;
 
-public class Deck {
-
+public class Deck 
+{
+	private ArrayList<Card> unDealt;
+	private ArrayList<Card> Dealt;
+	
+	public boolean isEmpty()
+	{
+		if 
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public Deck(String[]ranks,String[]suits,int[]pointValues)
+	{
+		for(int i = 0; i < ranks.length;i++)
+		{
+			for(int j= 0; j < suits.length;j++)
+			{
+				unDealt.add(new Card(ranks[i],suits[j],pointValues[i]));
+			}
+		}
+	}
 }
